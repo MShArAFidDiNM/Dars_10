3e72cb88e32bb306504ab6402277dfa9e558fe95
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-12 10:54:11 +0500

    Ikki o'lchamli massiv

--- dars10/dars10/Program.cs
+++ dars10/dars10/Program.cs
@@ -10,6 +10,50 @@ namespace dars10
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            int[][] arr = new int[5][];          
+            int diaganalsum = 0; 
+            int diaganalsum2 = 0; 
+            int arraylengthsum = 0; 
+            int sum = 0;
+         
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new int[5];
+
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    arr[i][j] = rand.Next(1, 10);
+                    Console.Write(arr[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0;i < arr.Length; i++)
+            {
+                for(int j = 0;j < arr.Length; j++)
+                {
+                    if (i + j == arr.Length - 1 ) diaganalsum += arr[i][j];
+                    if (i == 0 ) sum += arr[i][j];
+                    if (i == j ) diaganalsum2+= arr[i][j];
+                    if (i == arr.Length - 1 ) arraylengthsum += arr[i][j];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("sum : " + sum);
+            Console.WriteLine("Diaganalsum : " + diaganalsum);
+            bool check = false;
+            if (diaganalsum > sum) check = true;
+            Console.WriteLine(check); 
+            Console.WriteLine();
+            Console.WriteLine("Array Length Sum : " + arraylengthsum);
+            Console.WriteLine("Diaganalsum : " + diaganalsum2);
+            bool check1 = false;
+            if (diaganalsum2 > arraylengthsum) check = true;
+            Console.WriteLine(check);
+
+            Console.ReadLine();
         }
     }
 }
